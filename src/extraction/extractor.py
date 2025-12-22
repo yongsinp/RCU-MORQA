9c03643d926bccb074052a77d0848c891ff4e458
Yongsin Park <yongsin.nlp@gmail.com>
2025-12-22 01:02:55 -0800

    Refactored `RuleBasedExtractor`
     - Made `RuleBasedExtractor.extract_questions()` return `Document`
     - Added `Extractor._get_new_document()`
     - Added `Extractor._create_annotation()`
     - Made `RuleBasedExtractor._create_annotation()` call `Extractor._create_annotation()`
     - Moved implicit question logic to `Extractor._add_implicit_questions()`
     - Moved id assigning logic to `Extractor._assign_ids()`
     - Turned `RuleBasedExtractor._extract_by_punctuation()` and `RuleBasedExtractor._extract_by_please()` into a static method
     - Added logger to the base `Extractor` class
     - Added log for debugging

--- src/extraction/extractor.py
+++ src/extraction/extractor.py
@@ -1,6 +1,8 @@
+import copy
+import logging
 from abc import ABC, abstractmethod
 
-from src.preprocess.data import Document, Annotation
+from src.preprocess.data import Document, Annotation, Attribute, Label, IMPLICIT_QUESTTYP
 
 
 class Extractor(ABC):
@@ -8,6 +10,67 @@ class Extractor(ABC):
 
     def __init__(self, model_name: str):
         self.model_name = model_name
+        self.logger = logging.getLogger(self.model_name)
+
+    @staticmethod
+    def _get_new_document(document: Document) -> Document:
+        """Creates a new Document with empty annotations and responses.
+
+        Args:
+            document: The original Document.
+        Returns:
+            A new Document with empty annotations and responses.
+        """
+        new_document = copy.deepcopy(document)
+
+        # Clear existing annotations and responses
+        new_document.annotations = {k: [] for k in new_document.annotations}
+        new_document.responses = []
+
+        return new_document
+
+    @staticmethod
+    def _create_annotation(text: str, start: int, end: int) -> Annotation:
+        """Creates a default question Annotation.
+
+        `doc`, `ent_id`, and `att.id` have empty values and should be assigned later.
+
+        Args:
+            id: The document ID.
+            text: The question text.
+            start: The start index of the question in the document.
+            end: The end index of the question in the document.
+        Returns:
+            The created Annotation.
+        """
+        return Annotation(
+            att=Attribute(text=text),
+            start=start,
+            end=end,
+            doc="",
+            ent_id="",
+            label=Label.QUESTION
+        )
+
+    @staticmethod
+    def _add_implicit_questions(new_document: Document) -> None:
+        """Adds implicit question annotations based on the first word of the query."""
+        key = 'query_title' if new_document.query_title else 'query_content'
+        first_word = getattr(new_document, key).split()[0]
+        start, end = 0, len(first_word)
+        for questtyp in IMPLICIT_QUESTTYP:
+            ann = Extractor._create_annotation(first_word, start, end)
+            ann.att.is_implicit = True
+            ann.att.questtyp = questtyp
+            new_document.annotations[key].append(ann)
+
+    @staticmethod
+    def _assign_ids(document: Document) -> None:
+        """Assigns unique IDs to each annotation in the document."""
+        for i, question in enumerate(document.questions, start=1):
+            question.doc = f"{document.post_id}.ann"
+            question.ent_id = f"T{i}"
+            question.att.id = str(i)
 
     @abstractmethod
     def extract_questions(self, document: Document) -> list[Annotation]:
--- src/extraction/rule.py
+++ src/extraction/rule.py
@@ -1,13 +1,13 @@
 import os
 from collections import Counter
-from typing import Callable
+from typing import Callable, override
 
 import spacy
 from spacy.tokens import Doc, Span
 
 from src.eval.eval import is_overlapping
 from src.extraction.extractor import Extractor
-from src.preprocess.data import Document, Annotation, Attribute, Label, IMPLICIT_QUESTTYP
+from src.preprocess.data import Document, Annotation
 from src.util.io import read_json
 
 
@@ -40,32 +40,7 @@ class RuleBasedExtractor(Extractor):
         self.model = spacy.load(self.model_name)
 
     @staticmethod
-    def _create_annotation(question_span: Span) -> Annotation:
-        """
-        Creates a default question Annotation from a spaCy Span.
-
-        `doc`, `ent_id`, and `att.id` have empty values and should be assigned later.
-
-        Args:
-            question_span: The spaCy span representing the question.
-
-        Returns:
-            The created Annotation.
-        """
-        text = question_span.text.strip()
-        start = question_span.start_char
-        end = question_span.end_char
-        return Annotation(
-            att=Attribute(text=text),
-            start=start,
-            end=end,
-            doc="",
-            ent_id="",
-            label=Label.QUESTION
-        )
-
-    @classmethod
-    def _extract_by_punctuation(cls, doc: Doc) -> list[Annotation]:
+    def _extract_by_punctuation(doc: Doc) -> list[Annotation]:
         """
         Extracts questions based on punctuation marks.
 
@@ -88,14 +63,14 @@ class RuleBasedExtractor(Extractor):
                     current_token = doc[i]
 
                     # Hard punctuation boundaries
-                    if current_token.text in cls.SENTENCE_BOUNDARIES:
+                    if current_token.text in RuleBasedExtractor.SENTENCE_BOUNDARIES:
                         start_idx = i + 1  # Question starts after the boundary
                         break
 
                     # Comma + question starters
                     if current_token.text == ",":
                         next_token = doc[i + 1]
-                        if next_token.tag_ in cls.QUESTION_STARTERS:
+                        if next_token.tag_ in RuleBasedExtractor.QUESTION_STARTERS:
                             start_idx = i + 1
                             break
 
@@ -109,12 +84,12 @@ class RuleBasedExtractor(Extractor):
 
                 if start_idx < end_idx:
                     question_span = doc[start_idx: end_idx]
-                    questions.append(cls._create_annotation(question_span))
+                    questions.append(RuleBasedExtractor._create_annotation(question_span))
 
         return questions
 
-    @classmethod
-    def _extract_by_please(cls, doc: Doc) -> list[Annotation]:
+    @staticmethod
+    def _extract_by_please(doc: Doc) -> list[Annotation]:
         """
         Extracts questions based on the word "please".
 
@@ -134,63 +109,67 @@ class RuleBasedExtractor(Extractor):
                 for i in range(start_idx, len(doc)):
                     current_token = doc[i]
 
-                    if current_token.text in cls.SENTENCE_BOUNDARIES:
+                    if current_token.text in RuleBasedExtractor.SENTENCE_BOUNDARIES:
                         question_span = doc[start_idx: current_token.i + 1]
-                        questions.append(cls._create_annotation(question_span))
+                        questions.append(RuleBasedExtractor._create_annotation(question_span))
                         break
                 else:
                     question_span = doc[start_idx:]
-                    questions.append(cls._create_annotation(question_span))
+                    questions.append(RuleBasedExtractor._create_annotation(question_span))
 
         return questions
 
+    @override
     @classmethod
-    def _extract_implicit_questions(cls, doc: Doc) -> list[Annotation]:
+    def _create_annotation(cls, question_span: Span) -> Annotation:
         """
-        Creates a list of three implicit question Annotations using the first word of the document.
+        Creates a default question Annotation from a spaCy Span.
 
-        Each annotation corresponds to a different implicit question type in IMPLICIT_QUESTTYP, in ascending order.
+        `doc`, `ent_id`, and `att.id` have empty values and should be assigned later.
 
         Args:
-            doc: The spaCy Doc object to extract questions from.
+            question_span: The spaCy span representing the question.
+
         Returns:
-            A list of implicit question Annotations. The `doc`, `ent_id`, and `att.id` fields of the Annotations are empty and should be assigned later.
+            The created Annotation.
         """
-        questions = []
+        text = question_span.text.strip()
+        start = question_span.start_char
+        end = question_span.end_char
+        return super()._create_annotation(text, start, end)
 
-        # Use first word, not token, as implicit question
-        end = next((token.i for token in doc if token.whitespace_), 0)
-        question_span = doc[:end + 1]
-        for questtyp in IMPLICIT_QUESTTYP:
-            ann = cls._create_annotation(question_span)
-            ann.att.is_implicit = True
-            ann.att.questtyp = questtyp
-            questions.append(ann)
+    def extract_questions(self, document: Document) -> Document:
+        """Extracts questions from the given document using rule-based methods.
 
-        return questions
+        Args:
+            document: The Document to extract questions from.
+        Returns:
+            A new Document with extracted question Annotations.
+        """
+        # Create a new document to return
+        new_document = self._get_new_document(document)
 
-    def extract_questions(self, document: Document) -> list[Annotation]:
-        docs = [self.model(text) for text in (document.query_title, document.query_content) if text]
-        questions = []
+        # Extract questions using rules
+        for key in ('query_title', 'query_content'):
+            if text := getattr(document, key).strip():
+                doc = self.model(text)
+                # Look for "?" first and then "please" if none found
+                extractions = self._extract_by_punctuation(doc) or self._extract_by_please(doc)
+                new_document.annotations[key].extend(sorted(extractions, key=lambda x: (x.start, x.end)))
 
-        for doc in docs:
-            # Primary: look for "?"
-            extractions = self._extract_by_punctuation(doc)
+        # Add implicit questions if no questions are extracted
+        if not any(new_document.annotations.values()):
+            self._add_implicit_questions(new_document)
 
-            # Fallback: use "please"
-            if not extractions:
-                extractions.extend(self._extract_by_please(doc))
+        # Assign IDs to extracted questions
+        self._assign_ids(new_document)
 
-            questions.extend(sorted(extractions, key=lambda x: (x.start, x.end)))
+        self.logger.debug(f"""
+Document ID: {document.post_id}
+    Gold Questions:\n\t\t{"\n\t\t".join(q.att.text for q in document.questions)}
+    Extracted Questions:\n\t\t{"\n\t\t".join(q.att.text for q in new_document.questions)}
+                """)
 
-        # Final fallback: use first word for implicit question
-        if not questions:
-            questions.extend(self._extract_implicit_questions(docs[0]))
+        return new_document
 
-        # Assign IDs
-        for i, question in enumerate(questions, start=1):
-            question.doc = f"{document.post_id}.ann"
-            question.ent_id = f"T{i}"
-            question.att.id = i
 
-        return questions
