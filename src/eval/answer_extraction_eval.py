4b96ddeea3f392c7aad516d48b25a13a7c2682a6
Yongsin Park <yongsin.nlp@gmail.com>
2025-12-27 16:04:10 -0800

    Fixed how the confusion matrix is calculated for answer extraction

--- src/eval/answer_extraction_eval.py
+++ src/eval/answer_extraction_eval.py
@@ -4,7 +4,7 @@ from functools import singledispatch
 from typing import Union
 
 from src.eval.eval import is_overlapping
-from src.preprocess.data import Document, Label
+from src.preprocess.data import Document, Label, Annotation
 from src.util.io import read_json
 
 
@@ -17,37 +17,38 @@ def eval(gold_data: Union[Document, list[Document]], pred_data: Union[Document, 
 
 @eval.register(Document)
 def _(gold_data: Document, pred_data: Document, exact_match: bool = False, match_attr: bool = False) -> Counter[str]:
+    def get_responses(data: Document) -> list[dict[str, Annotation]]:
+        responses = []
+        for response in data.responses:
+            answers = {}
+            for anns in response.annotations.values():
+                for ann in anns:
+                    if ann.label == Label.SHORTEST_ANSWER:
+                        # Add one answer per attribute ID
+                        answers[ann.att.id] = ann
+            responses.append(answers)
+        return responses
+
     metrics = Counter()
 
-    golds = [ann
-             for res in gold_data.responses
-             for anns in res.annotations.values()
-             for ann in anns
-             if ann.label == Label.SHORTEST_ANSWER]
-    preds = [ann
-             for res in pred_data.responses
-             for anns in res.annotations.values()
-             for ann in anns
-             if ann.label == Label.SHORTEST_ANSWER]
+    gold_responses = get_responses(gold_data)
+    pred_responses = get_responses(pred_data)
+    assert len(gold_responses) == len(pred_responses), "Number of responses in gold and predicted data do not match."
 
-    pred_candidates = set(preds)
-    for gold in golds:
-        for pred in pred_candidates:
-            if is_overlapping(gold, pred, exact_match=exact_match, match_attr=match_attr):
+    for gold_response, pred_response in zip(gold_responses, pred_responses):
+        for id_, gold_ann in gold_response.items():
+            pred_ann = pred_response.get(id_)
+            if pred_ann is None:
+                metrics['FN'] += 1
+            elif is_overlapping(gold_ann, pred_ann, exact_match=exact_match, match_attr=match_attr):
                 metrics['TP'] += 1
-                pred_candidates.remove(pred)
-                break
-        else:
-            metrics['FN'] += 1
+            else:
+                metrics['FN'] += 1
+                metrics['FP'] += 1
 
-    gold_candidates = set(golds)
-    for pred in preds:
-        for gold in gold_candidates:
-            if is_overlapping(gold, pred, exact_match=exact_match, match_attr=match_attr):
-                gold_candidates.remove(gold)
-                break
-        else:
-            metrics['FP'] += 1
+        for id_, pred_ann in pred_response.items():
+            if id_ not in gold_response:
+                metrics['FP'] += 1
 
     return metrics
 
