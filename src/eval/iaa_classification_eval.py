af151fb81dd45e54b2efe1c02ed1f532652c5114
Yongsin Park <yongsin.nlp@gmail.com>
2026-01-12 01:12:10 -0800

    Added iaa_classification_eval.py

---
+++ src/eval/iaa_classification_eval.py
@@ -0,0 +1,195 @@
+import os
+from functools import singledispatch
+from typing import Any, Union
+
+from sklearn.metrics import precision_recall_fscore_support, classification_report
+
+from src.preprocess.data import Document, QuestionType
+from src.util.io import read_json
+
+
+@singledispatch
+def eval(gold_data: Union[Document, list[Document]], pred_data: Union[Document, list[Document]]) -> dict[
+    str, list[Any]]:
+    """Evaluates the classification performance between gold and predicted data."""
+    ...
+
+
+@eval.register(Document)
+def _(gold_data: Document, pred_data: Document) -> dict[str, list[Any]]:
+    gold_problem = []
+    pred_problem = []
+    gold_test = []
+    pred_test = []
+    gold_treatment = []
+    pred_treatment = []
+    gold_follup = []
+    pred_follup = []
+    gold_severe = []
+    pred_severe = []
+    gold_conditional = []
+    pred_conditional = []
+
+    gold_responses = [ann
+                      for response in gold_data.responses
+                      for annotations in response.annotations.values()
+                      for ann in annotations]
+    pred_responses = [ann
+                      for response in pred_data.responses
+                      for annotations in response.annotations.values()
+                      for ann in annotations]
+
+    for gold, pred in zip(gold_responses, pred_responses):
+        gold_problem.append(gold.att.is_prob)
+        pred_problem.append(pred.att.is_prob)
+        gold_test.append(gold.att.is_test)
+        pred_test.append(pred.att.is_test)
+        gold_treatment.append(gold.att.is_treat)
+        pred_treatment.append(pred.att.is_treat)
+        gold_follup.append(gold.att.is_follup)
+        pred_follup.append(pred.att.is_follup)
+        gold_severe.append(gold.att.is_severe)
+        pred_severe.append(pred.att.is_severe)
+        gold_conditional.append(gold.att.is_conditional)
+        pred_conditional.append(pred.att.is_conditional)
+
+    return {
+        'gold_problem': gold_problem,
+        'pred_problem': pred_problem,
+        'gold_test': gold_test,
+        'pred_test': pred_test,
+        'gold_treatment': gold_treatment,
+        'pred_treatment': pred_treatment,
+        'gold_follup': gold_follup,
+        'pred_follup': pred_follup,
+        'gold_severe': gold_severe,
+        'pred_severe': pred_severe,
+        'gold_conditional': gold_conditional,
+        'pred_conditional': pred_conditional,
+    }
+
+
+@eval.register(list)
+def _(gold_data: list[Document], pred_data: list[Document]) -> dict[str, list[Any]]:
+    gold_problem = []
+    pred_problem = []
+    gold_test = []
+    pred_test = []
+    gold_treatment = []
+    pred_treatment = []
+    gold_follup = []
+    pred_follup = []
+    gold_severe = []
+    pred_severe = []
+    gold_conditional = []
+    pred_conditional = []
+
+    for gold_doc, pred_doc in zip(gold_data, pred_data):
+        result = eval(gold_doc, pred_doc)
+        gold_problem.extend(result['gold_problem'])
+        pred_problem.extend(result['pred_problem'])
+        gold_test.extend(result['gold_test'])
+        pred_test.extend(result['pred_test'])
+        gold_treatment.extend(result['gold_treatment'])
+        pred_treatment.extend(result['pred_treatment'])
+        gold_follup.extend(result['gold_follup'])
+        pred_follup.extend(result['pred_follup'])
+        gold_severe.extend(result['gold_severe'])
+        pred_severe.extend(result['pred_severe'])
+        gold_conditional.extend(result['gold_conditional'])
+        pred_conditional.extend(result['pred_conditional'])
+
+    return {
+        'gold_problem': gold_problem,
+        'pred_problem': pred_problem,
+        'gold_test': gold_test,
+        'pred_test': pred_test,
+        'gold_treatment': gold_treatment,
+        'pred_treatment': pred_treatment,
+        'gold_follup': gold_follup,
+        'pred_follup': pred_follup,
+        'gold_severe': gold_severe,
+        'pred_severe': pred_severe,
+        'gold_conditional': gold_conditional,
+        'pred_conditional': pred_conditional,
+    }
+
+
+if __name__ == '__main__':
+    # Paths
+    gold_path = "../../data/rcu-en/"
+    pred_path = "../../out/iaa_classification/"
+
+    # File names
+    datasets = [
+        "iiyi",
+        "woundcare",
+    ]
+    splits = [
+        "train_gold",
+        "valid_gold",
+    ]
+    files = [f"{dataset}_{split}.json" for dataset in datasets for split in splits]
+
+    # Model names
+    models = [dir_name for dir_name in os.listdir(pred_path) if os.path.isdir(os.path.join(pred_path, dir_name))]
+
+    for model in models:
+        print(f"Model: {model}")
+        for file in files:
+            print(f"\tFile: {file}")
+
+            gold_file = os.path.join(gold_path, file)
+            pred_file = os.path.join(pred_path, model, file)
+            gold_data = [Document.from_dict(doc) for doc in read_json(gold_file)]
+            pred_data = [Document.from_dict(doc) for doc in read_json(pred_file)]
+
+            result = eval(gold_data, pred_data)
+
+            # Problem metrics
+            print("=== Problem ===")
+            p, r, f1, _ = precision_recall_fscore_support(
+                result['gold_problem'], result['pred_problem'], average='weighted', zero_division=0
+            )
+            print(f"Precision: {p:.4f}, Recall: {r:.4f}, F1: {f1:.4f}")
+            print(classification_report(result['gold_problem'], result['pred_problem']))
+
+            # Test metrics
+            print("=== Test ===")
+            p, r, f1, _ = precision_recall_fscore_support(
+                result['gold_test'], result['pred_test'], average='weighted', zero_division=0
+            )
+            print(f"Precision: {p:.4f}, Recall: {r:.4f}, F1: {f1:.4f}")
+            print(classification_report(result['gold_test'], result['pred_test']))
+
+            # Treatment metrics
+            print("=== Treatment ===")
+            p, r, f1, _ = precision_recall_fscore_support(
+                result['gold_treatment'], result['pred_treatment'], average='weighted', zero_division=0
+            )
+            print(f"Precision: {p:.4f}, Recall: {r:.4f}, F1: {f1:.4f}")
+            print(classification_report(result['gold_treatment'], result['pred_treatment']))
+
+            # Follow-up metrics
+            print("=== Follow-up ===")
+            p, r, f1, _ = precision_recall_fscore_support(
+                result['gold_follup'], result['pred_follup'], average='weighted', zero_division=0
+            )
+            print(f"Precision: {p:.4f}, Recall: {r:.4f}, F1: {f1:.4f}")
+            print(classification_report(result['gold_follup'], result['pred_follup']))
+
+            # Severe metrics
+            print("=== Severe ===")
+            p, r, f1, _ = precision_recall_fscore_support(
+                result['gold_severe'], result['pred_severe'], average='weighted', zero_division=0
+            )
+            print(f"Precision: {p:.4f}, Recall: {r:.4f}, F1: {f1:.4f}")
+            print(classification_report(result['gold_severe'], result['pred_severe']))
+
+            # Conditional metrics
+            print("=== Conditional ===")
+            p, r, f1, _ = precision_recall_fscore_support(
+                result['gold_conditional'], result['pred_conditional'], average='weighted', zero_division=0
+            )
+            print(f"Precision: {p:.4f}, Recall: {r:.4f}, F1: {f1:.4f}")
+            print(classification_report(result['gold_conditional'], result['pred_conditional']))
